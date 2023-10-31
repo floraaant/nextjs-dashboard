c8e3adcf9f156885e040a7665e4c2b1d64b6c5cb
Florant Hauguel <floraaant@macbook-pro-de-florant.home>
2023-10-31 23:46:13 +0100

    Fin chapitre 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,14 +12,17 @@ const InvoiceSchema = z.object({
     status: z.enum(['pending', 'paid']),
     date: z.string(),
 });
+
 const CreateInvoice = InvoiceSchema.omit({id: true, date: true});
+const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true });
+
 export async function createInvoice(formData: FormData){
     const {customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-    const amountInCents = amount * 100;
+    const amountInCents = parseFloat((amount * 100).toFixed(2));
     const date = new Date().toISOString().split('T')[0];
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date) 
@@ -28,3 +31,27 @@ export async function createInvoice(formData: FormData){
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = parseFloat((amount * 100).toFixed(2));
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+  
+  export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }
