105bf19b341c2a79788d484dad81b8f0e3828166
Florant Hauguel <floraaant@macbook-pro-de-florant.home>
2023-11-05 17:42:12 +0100

    Pagination dans customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,24 +1,47 @@
 import Table from '@/app/ui/customers/table';
 import { Metadata } from 'next';
-import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
- 
+import { fetchCustomersPages, fetchFilteredCustomers } from '@/app/lib/data';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import Pagination from '@/app/ui/invoices/pagination';
+
+
 export const metadata: Metadata = {
   title: 'Customers',
 };
- 
+
 export default async function Page({
-  searchParams, 
+  searchParams,
 }: {
   searchParams?: {
-      query?: string;
+    query?: string;
+    page?: string;
   };
-}){  
+}) {
   const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
 
-  const customers = await fetchFilteredCustomers(query);
+  const totalPages = await fetchCustomersPages(query);
+
+
   return (
     <div className="w-full">
-      <Table customers={customers} />
-    </div>
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>
+          Customers
+        </h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div >
   );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -78,7 +78,7 @@ export async function fetchCardData() {
   }
 }
 
-const ITEMS_PER_PAGE = 6;
+const ITEMS_PER_PAGE = 5;
 export async function fetchFilteredInvoices(
   query: string,
   currentPage: number
@@ -188,8 +188,10 @@ export async function fetchCustomers() {
   }
 }
 
-export async function fetchFilteredCustomers(query: string) {
+export async function fetchFilteredCustomers(query: string, currentPage: number) {
   noStore();
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+
   try {
     const data = await sql<CustomersTable>`
 		SELECT
@@ -207,6 +209,7 @@ export async function fetchFilteredCustomers(query: string) {
         customers.email ILIKE ${`%${query}%`}
 		GROUP BY customers.id, customers.name, customers.email, customers.image_url
     ORDER BY total_invoices DESC
+    LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
 	  `;
 
     const customers = data.rows.map((customer) => ({
@@ -219,6 +222,24 @@ export async function fetchFilteredCustomers(query: string) {
   } catch (err) {
     console.error("Database Error:", err);
     throw new Error("Failed to fetch customer table.");
+  }
+}
+
+export async function fetchCustomersPages(query: string) {
+  noStore();
+  try {
+    const count = await sql`SELECT COUNT(*) 
+    FROM customers
+  WHERE
+    customers.name ILIKE ${`%${query}%`} OR
+      customers.email ILIKE ${`%${query}%`}
+  `;
+
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch total number of invoices.");
   }
 }
 
